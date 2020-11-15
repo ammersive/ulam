0b151fc1569b565c2df0a4ccb01d71d8c1503386
Sophie Stammers <sophie.stammers@gmail.com>
2020-11-15 09:49:55 +0000

    hilarious non optimal attempt use arrays next time

--- app.js
+++ app.js
@@ -13,34 +13,46 @@
     } 
     return isPrime;
   }  
+  //build spans : working  
+  // for (var i=0;i<10000;i+=1) {
+  //   pixels.append(d.createElement("span"));
+  // }  
 
-  // build spans  
-  for (var i=0;i<10000;i+=1) {
-      pixels.append(d.createElement("span"));
-  }  
-  // get main
-  let main = d.getElementById("pixels");
+  let fragment = d.createDocumentFragment();
 
-  // keep track of which spans are primeList
-  let primeList = [];
+  for (var i=0;i<100;i+=1) {
+    span = d.createElement("span")
+    if (prime(i)) {
+    // if (i % 2 === 0){  
+      span.style.backgroundColor = "red";
+    } 
+    fragment.append(span);
+  } 
 
-//   // Prime number function
-//   let prime = num => {
-//     let isPrime = true; 
-//     if (num <= 1) {
-//       isPrime = false;
-//     } else { 
-//       for (i = 2; i <= (num / 2); i += 1) {
-//         if (num % i === 0) {
-//           isPrime = false;
-//         }
-//       }
-//     } 
-//     return isPrime;
-//   }
+  pixels.append(fragment);
 
-//   if (prime(indexOf("span"))) {
+  // // get main
+  // let main = d.getElementById("pixels");
 
-//   }
+  
+  // let numbers = [];  
+
+  // let fragment = d.createDocumentFragment();
+  // let main = d.getElementById("main");
+
+  // // build spans  
+  // for (var i=0; i<10000; i+=1) {    
+  //   numbers.push(i);          
+  // } 
+  // console.log(numbers);
+
+  // // let span = d.createElement("span");
+  // // console.log(numbers);
+
+
+
+  // // fragment.append(span);
+  // // main.append(fragment);
+
 
 })(document);
