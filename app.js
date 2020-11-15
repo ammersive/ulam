9ca62909d211f61c211bf30ff7c1ed1e04f98152
Sophie Stammers <sophie.stammers@gmail.com>
2020-11-15 10:48:24 +0000

    The hoisting of var- a lesson learned the hard way

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (d => {
 
-  
+
   // Prime number function
   let prime = num => {
     let isPrime = true; 
@@ -29,27 +29,27 @@
 
   let spanArray = []; 
 
-  for (var i = 1; i < 10000; i += 1) {
+  for (let j = 1; j < 10000; j += 1) {
     span = d.createElement("span");
     spanArray.push(span);    
   }
   console.log(spanArray);
 
-  for (var i = 1; i < 100; i += 1) {
+  for (let k = 1; k < 100; k += 1) {
     // fine
-    if (i % 3 === 0) {
+    if (k % 3 === 0) {
     // NOT FINE  
     // if (true) {
     // if (i % 3 === 0) {
     // if (prime(i)){
-      console.log(prime(i)); // fine
+      console.log(prime(k)); // fine
       // spanArray[i].classList.add("prime-colour");
     }
   }  
   // console.log(spanArray);
 
   let fragment = d.createDocumentFragment();
-  for (var i = 1; i < spanArray.length - 1; i += 1) {
+  for (let i = 1; i < spanArray.length - 1; i += 1) {
     fragment.append(spanArray[i]);
   }  
 
