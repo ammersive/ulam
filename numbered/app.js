df0bad073d8fc9284e979a321172bea130c1a203
Sophie Stammers <sophie.stammers@gmail.com>
2020-11-16 18:49:31 +0000

    Titied up js code on numbered and numbered-coloured

--- numbered/app.js
+++ numbered/app.js
@@ -1,9 +1,8 @@
 (d => {
 
-
   // Build an array of span elements
-  let gridLen = 20;
-  let gridSize = gridLen * gridLen;
+  let gridLength = 20;
+  let gridSize = gridLength * gridLength;
   let spanArray = []; 
 
   for (let i = 0; i < gridSize; i += 1) {
@@ -11,58 +10,61 @@
     spanArray.push(span);    
   }
 
-  // center span where print head starts
-  let currentIndex = (gridSize / 2) + (gridLen / 2);
+  // The position of the print head, starting in the center
+  let currentIndex = (gridSize / 2) + (gridLength / 2);  
+
+  // For colouring paths
   let iString = "";
-
-  // Difference in span index as print head moves, we'll cycle through the array with modulus to get our spiral
-  let north = -gridLen;
-  let south = +gridLen;
-  let east = 1;
-  let west = -1;
-  let directionalIndexSteps = [north, west, south, east];
-
-  let colours = ["red", "orange", "yellow", "green", "paleblue", "blue", "purple", "brown"];
+  let colours = ["#A0246B", "#D45D6C", "#EEC531", "#F2E260", "#BEF577", "#6AE93F", "#39DDF4", "#289DF4"];
   let currentColour = 0;
 
-  // Current direction of travel for print head (used to determine direction of travel in directionalIndexSteps)
+  // Directions: differences in span indexes as the print head moves
+  let up = -gridLength;
+  let down = +gridLength;
+  let right = 1;
+  let left = -1;
+  let directions = [up, left, down, right];
+
+  // Current direction of travel of the print head
   let currentDirection = 0;  
-  // spans left to check before turning
+  // Spans left to check before turning
   let stepsRemaining = 1;
-  // "paths" will be each straight side of the spiral
+  // A path is the total straight side of the spiral before a turn is made
   let pathLength = 1;
-  // This accounts for initial tight spiralling required when paths are length 1
+  // This accounts for tight spiraling at the start of the loop (it should not increment until two directional turns)
   let pathsTraversed = -1;
 
   // Step through the natural numbers
   for (let i = 1; i <= gridSize; i += 1) {
 
-    // If steps remaining = 0, print head is at the end of a path. Log that with pathsTraversed.
+    // If no steps remain, the print head is at the end of a path. Log that with pathsTraversed.
     if (stepsRemaining === 0) {
       pathsTraversed += 1;
 
-      // When print head has traversed 2 paths/sides of the spiral, increase the path length 
+      // On paths of an odd number, increase the path length 
       if (pathsTraversed % 2 === 1) {
         pathLength += 1;
       }
-      // Change direction, reset steps remaining
+      // Change direction and reset steps remaining
       currentDirection = (currentDirection + 1) % 4;
       stepsRemaining = pathLength;
 
+      // Change span colour
       currentColour = (currentColour + 1) % colours.length;
     }
 
+    // Print a number to the current span 
     iString = String(i);
     spanArray[currentIndex].textContent = iString;
-
+    // Colour the current span according to current path colour
     spanArray[currentIndex].style.backgroundColor = colours[currentColour % colours.length];
 
-    // Move printhead one square in current direction
-    currentIndex += directionalIndexSteps[currentDirection];
+    // Move print head one square in current direction
+    currentIndex += directions[currentDirection];
     stepsRemaining -= 1;
   }
 
-  // Append span array elements to fragment, to send back to DOM 
+  // Append span array elements to a fragment, to send back to DOM in one go
   let fragment = d.createDocumentFragment();
   for (let i = 0; i < spanArray.length; i += 1) {
     fragment.append(spanArray[i]);
